21f2bf78cb5656f9e2b6fc4173c3a3c03d99c374
1ngVG <giannini.v@outlook.com>
2026-06-16 23:02:24 +0200

    UPDATE: programme

--- src/generated/schedule-manifest.js
+++ src/generated/schedule-manifest.js
@@ -4,12 +4,16 @@ const scheduleManifest = {
     "week": [
       "2026-08-26",
       "2026-08-27",
-      "2026-08-28"
+      "2026-08-28",
+      "2026-08-29",
+      "2026-08-30"
     ],
     "days": [
       "2026-08-26",
       "2026-08-27",
-      "2026-08-28"
+      "2026-08-28",
+      "2026-08-29",
+      "2026-08-30"
     ],
     "eventsByDay": {
       "2026-08-26": [
@@ -19,141 +23,141 @@ const scheduleManifest = {
           "startTime": "09:00",
           "endTime": "14:00",
           "title": "Arrival and Check-in",
-          "description": "We will be welcoming you at the town hall with gadgets and the info for your accommodation.",
-          "location": "Town Hall",
+          "description": "We will be welcoming you at the town hall with gadgets and the info for your accommodation",
+          "location": "Meeting point: Town Hall",
           "color": "#fecc08"
         },
         {
-          "id": "2026-08-26-15:00-2",
+          "id": "2026-08-26-16:00-2",
           "day": "2026-08-26",
-          "startTime": "15:00",
+          "startTime": "16:00",
           "endTime": "18:00",
           "title": "Tour of Amandola",
-          "description": "Guided tour of the town and its main attractions.",
-          "location": "Town Hall",
-          "color": "#458657"
-        },
-        {
-          "id": "2026-08-26-18:00-3",
-          "day": "2026-08-26",
-          "startTime": "18:00",
-          "endTime": "20:00",
-          "title": "Opening of the school",
-          "description": "Lectio Magistralis and ceremony",
-          "location": "TbD",
+          "description": "Guided tour of the town and its main attractions",
+          "location": "Meeting point: Town Hall",
           "color": "#fecc08"
         },
         {
-          "id": "2026-08-26-20:00-4",
+          "id": "2026-08-26-17:00-3",
           "day": "2026-08-26",
-          "startTime": "20:00",
-          "endTime": "22:00",
-          "title": "Dinner",
-          "description": "Enjoy a meal with fellow participants.",
-          "location": "Casa del Parco",
+          "startTime": "17:00",
+          "endTime": "19:00",
+          "title": "Socialisation game: get to know the students!",
+          "description": "Surprise",
+          "location": "",
           "color": "#458657"
         },
         {
-          "id": "2026-08-26-22:00-5",
+          "id": "2026-08-26-19:00-4",
+          "day": "2026-08-26",
+          "startTime": "19:00",
+          "endTime": "19:30",
+          "title": "Opening of the school",
+          "description": "",
+          "location": "Meeting point: TbD",
+          "color": "#9900ff"
+        },
+        {
+          "id": "2026-08-26-19:30-5",
+          "day": "2026-08-26",
+          "startTime": "19:30",
+          "endTime": "20:30",
+          "title": "In defence of Europe",
+          "description": "Lectio magistralis: Speakers will be revealed soon",
+          "location": "Meeting point: La Collegiata",
+          "color": "#0000ff"
+        },
+        {
+          "id": "2026-08-26-20:30-6",
+          "day": "2026-08-26",
+          "startTime": "20:30",
+          "endTime": "22:00",
+          "title": "Welcome typical dinner",
+          "description": "",
+          "location": "Meeting point: Casa del Parco",
+          "color": "#ff9900"
+        },
+        {
+          "id": "2026-08-26-22:00-7",
           "day": "2026-08-26",
           "startTime": "22:00",
           "endTime": "00:00",
-          "title": "Socialising Activity",
-          "description": "Relax and mingle with other attendees.",
-          "location": "TbD",
-          "color": "#fecc08"
+          "title": "Socialising activity",
+          "description": "Surprise",
+          "location": "Meeting point: Town Hall",
+          "color": "#458657"
         }
       ],
       "2026-08-27": [
         {
-          "id": "2026-08-27-09:00-6",
+          "id": "2026-08-27-09:15-8",
           "day": "2026-08-27",
-          "startTime": "09:00",
-          "endTime": "10:15",
-          "title": "Lecture 1 - History of EU",
-          "description": "Introduction to the history of the European Union.",
-          "location": "TbD",
-          "color": "#458657"
+          "startTime": "09:15",
+          "endTime": "10:30",
+          "title": "History of EU and European Integration",
+          "description": "Lecture by: Speakers will be revealed soon",
+          "location": "Meeting point: La Collegiata",
+          "color": "#0000ff"
         },
         {
-          "id": "2026-08-27-10:15-7",
+          "id": "2026-08-27-10:30-9",
           "day": "2026-08-27",
-          "startTime": "10:15",
-          "endTime": "11:30",
-          "title": "Lecture 2 - Working for the EU institutions",
-          "description": "Overview of career opportunities within EU institutions.",
-          "location": "TbD",
-          "color": "#fecc08"
-        },
-        {
-          "id": "2026-08-27-11:30-8",
-          "day": "2026-08-27",
-          "startTime": "11:30",
-          "endTime": "11:45",
+          "startTime": "10:30",
+          "endTime": "11:00",
           "title": "Break",
-          "description": "Short break for refreshments.",
-          "location": "TbD",
-          "color": "#458657"
+          "description": "",
+          "location": "",
+          "color": "#ff9900"
         },
         {
-          "id": "2026-08-27-11:45-9",
+          "id": "2026-08-27-11:00-10",
           "day": "2026-08-27",
-          "startTime": "11:45",
+          "startTime": "11:00",
           "endTime": "13:00",
           "title": "Presentation of the LUISS project",
-          "description": "Detailed presentation of the LUISS project.",
-          "location": "TbD",
-          "color": "#fecc08"
+          "description": "",
+          "location": "Meeting point: La Collegiata",
+          "color": "#0000ff"
         },
         {
-          "id": "2026-08-27-13:00-10",
+          "id": "2026-08-27-13:00-11",
           "day": "2026-08-27",
           "startTime": "13:00",
           "endTime": "14:30",
           "title": "Lunch",
-          "description": "Enjoy a meal with fellow participants.",
-          "location": "Casa del Parco",
-          "color": "#458657"
+          "description": "",
+          "location": "Meeting point: Casa del Parco",
+          "color": "#ff9900"
         },
         {
-          "id": "2026-08-27-14:30-11",
+          "id": "2026-08-27-14:30-12",
           "day": "2026-08-27",
           "startTime": "14:30",
-          "endTime": "15:45",
-          "title": "Lecture 3",
-          "description": "Continuation of the lecture series.",
-          "location": "TbD",
-          "color": "#fecc08"
+          "endTime": "16:00",
+          "title": "Working for the EU institutions - policies in action. Speed sessions",
+          "description": "Lecture by: Speakers will be revealed soon",
+          "location": "Meeting point: La Collegiata",
+          "color": "#0000ff"
         },
         {
-          "id": "2026-08-27-15:45-12",
+          "id": "2026-08-27-16:00-13",
           "day": "2026-08-27",
-          "startTime": "15:45",
-          "endTime": "17:00",
-          "title": "Workshop / Lecture 4",
-          "description": "Interactive session or lecture.",
-          "location": "TbD",
-          "color": "#458657"
-        },
-        {
-          "id": "2026-08-27-17:00-13",
-          "day": "2026-08-27",
-          "startTime": "17:00",
+          "startTime": "16:00",
           "endTime": "18:30",
-          "title": "Break",
-          "description": "Short break for refreshments.",
-          "location": "TbD",
-          "color": "#fecc08"
+          "title": "Lecture - topic to be confirmed",
+          "description": "",
+          "location": "Meeting point: La Collegiata",
+          "color": "#0000ff"
         },
         {
           "id": "2026-08-27-18:30-14",
           "day": "2026-08-27",
           "startTime": "18:30",
           "endTime": "20:00",
-          "title": "Public Panel 1",
-          "description": "Discussion with guest speakers.",
-          "location": "TbD",
-          "color": "#458657"
+          "title": "Public panel: Europe in the Atlantic and the world",
+          "description": "Speakers will be revealed soon",
+          "location": "Meeting point: Town Hall",
+          "color": "#9900ff"
         },
         {
           "id": "2026-08-27-20:00-15",
@@ -161,70 +165,224 @@ const scheduleManifest = {
           "startTime": "20:00",
           "endTime": "21:30",
           "title": "Dinner",
-          "description": "Enjoy a meal with fellow participants.",
-          "location": "Casa del Parco",
-          "color": "#fecc08"
+          "description": "",
+          "location": "Meeting point: Casa del Parco",
+          "color": "#ff9900"
         },
         {
           "id": "2026-08-27-21:30-16",
           "day": "2026-08-27",
           "startTime": "21:30",
-          "endTime": "23:00",
-          "title": "Karaoke Night",
-          "description": "Fun and entertainment with fellow participants.",
-          "location": "TbD",
+          "endTime": "00:00",
+          "title": "Karaoke night",
+          "description": "",
+          "location": "Meeting point: TbD",
           "color": "#458657"
         }
       ],
       "2026-08-28": [
         {
-          "id": "2026-08-28-09:00-17",
+          "id": "2026-08-28-09:15-17",
           "day": "2026-08-28",
-          "startTime": "09:00",
-          "endTime": "10:15",
-          "title": "Lecture 4",
-          "description": "Continuation of the lecture series.",
-          "location": "TbD",
-          "color": "#fecc08"
-        },
-        {
-          "id": "2026-08-28-10:15-18",
-          "day": "2026-08-28",
-          "startTime": "10:15",
+          "startTime": "09:15",
           "endTime": "10:30",
-          "title": "Break",
-          "description": "Short break for refreshments.",
-          "location": "TbD",
-          "color": "#458657"
+          "title": "Lecture - topic to be confirmed",
+          "description": "",
+          "location": "Meeting point: La Collegiata",
+          "color": "#0000ff"
         },
         {
-          "id": "2026-08-28-10:30-19",
+          "id": "2026-08-28-10:30-18",
           "day": "2026-08-28",
           "startTime": "10:30",
-          "endTime": "13:00",
-          "title": "Simulation Exercise",
-          "description": "Interactive simulation exercise.",
-          "location": "TbD",
-          "color": "#fecc08"
+          "endTime": "11:00",
+          "title": "Break",
+          "description": "",
+          "location": "",
+          "color": "#ff9900"
         },
         {
-          "id": "2026-08-28-13:00-20",
+          "id": "2026-08-28-11:00-19",
           "day": "2026-08-28",
+          "startTime": "11:00",
+          "endTime": "12:30",
+          "title": "EU Digital Policies",
+          "description": "Lecture by: Speakers will be revealed soon",
+          "location": "Meeting point: La Collegiata",
+          "color": "#0000ff"
+        },
+        {
+          "id": "2026-08-28-12:30-20",
+          "day": "2026-08-28",
+          "startTime": "12:30",
+          "endTime": "14:00",
+          "title": "Lunch",
+          "description": "",
+          "location": "Meeting point: Casa del Parco",
+          "color": "#ff9900"
+        },
+        {
+          "id": "2026-08-28-14:00-21",
+          "day": "2026-08-28",
+          "startTime": "14:00",
+          "endTime": "16:30",
+          "title": "Career coffee",
+          "description": "Meet the professional you want to become",
+          "location": "Meeting point: Town Hall",
+          "color": "#9900ff"
+        },
+        {
+          "id": "2026-08-28-16:30-22",
+          "day": "2026-08-28",
+          "startTime": "16:30",
+          "endTime": "18:30",
+          "title": "Simulation exercise",
+          "description": "",
+          "location": "Meeting point: La Collegiata",
+          "color": "#0000ff"
+        },
+        {
+          "id": "2026-08-28-18:30-23",
+          "day": "2026-08-28",
+          "startTime": "18:30",
+          "endTime": "20:30",
+          "title": "Public panel: EU security and defence",
+          "description": "Lecture by: Speakers will be revealed soon",
+          "location": "Meeting point: Town Hall",
+          "color": "#9900ff"
+        },
+        {
+          "id": "2026-08-28-20:30-24",
+          "day": "2026-08-28",
+          "startTime": "20:30",
+          "endTime": "22:00",
+          "title": "Dinner",
+          "description": "",
+          "location": "Meeting point: Casa del Parco",
+          "color": "#ff9900"
+        },
+        {
+          "id": "2026-08-28-22:00-25",
+          "day": "2026-08-28",
+          "startTime": "22:00",
+          "endTime": "00:00",
+          "title": "Party in Piazza",
+          "description": "",
+          "location": "Meeting point: Town Hall",
+          "color": "#458657"
+        }
+      ],
+      "2026-08-29": [
+        {
+          "id": "2026-08-29-09:00-26",
+          "day": "2026-08-29",
+          "startTime": "09:00",
+          "endTime": "11:00",
+          "title": "Departure for the mountain",
+          "description": "",
+          "location": "Meeting point: TbD",
+          "color": "#b7b7b7"
+        },
+        {
+          "id": "2026-08-29-11:00-27",
+          "day": "2026-08-29",
+          "startTime": "11:00",
+          "endTime": "13:00",
+          "title": "The difficult path to the Union, from Ukraine to Turkey.",
+          "description": "Mountain Lecture by: Speakers will be revealed soon",
+          "location": "Meeting point: Sibillini",
+          "color": "#0000ff"
+        },
+        {
+          "id": "2026-08-29-13:00-28",
+          "day": "2026-08-29",
           "startTime": "13:00",
+          "endTime": "15:00",
+          "title": "Lunch",
+          "description": "",
+          "location": "",
+          "color": "#ff9900"
+        },
+        {
+          "id": "2026-08-29-15:00-29",
+          "day": "2026-08-29",
+          "startTime": "15:00",
+          "endTime": "16:00",
+          "title": "Return from the mountain",
+          "description": "",
+          "location": "",
+          "color": ""
+        },
+        {
+          "id": "2026-08-29-16:00-30",
+          "day": "2026-08-29",
+          "startTime": "16:00",
+          "endTime": "18:00",
+          "title": "CV Activity",
+          "description": "How to create a perfect CV!",
+          "location": "Meeting point: Casa del Parco",
+          "color": "#9900ff"
+        },
+        {
+          "id": "2026-08-29-18:30-31",
+          "day": "2026-08-29",
+          "startTime": "18:30",
+          "endTime": "20:00",
+          "title": "Public Panel: L’Europa e i suoi territori: la politica di Coesione",
+          "description": "Speakers will be revealed soon",
+          "location": "Meeting point: Town Hall",
+          "color": "#9900ff"
+        },
+        {
+          "id": "2026-08-29-20:00-32",
+          "day": "2026-08-29",
+          "startTime": "20:00",
+          "endTime": "00:00",
+          "title": "Cena al contrario",
+          "description": "Backwards dinner organised by Amandola Youth Organisation, CACUAM",
+          "location": "Meeting point: TbD",
+          "color": "#458657"
+        }
+      ],
+      "2026-08-30": [
+        {
+          "id": "2026-08-30-10:00-33",
+          "day": "2026-08-30",
+          "startTime": "10:00",
+          "endTime": "11:00",
+          "title": "Checkout",
+          "description": "",
+          "location": "",
+          "color": "#b7b7b7"
+        },
+        {
+          "id": "2026-08-30-11:00-34",
+          "day": "2026-08-30",
+          "startTime": "11:00",
+          "endTime": "12:30",
+          "title": "Book presentation and farewell ceremony",
+          "description": "",
+          "location": "",
+          "color": "#9900ff"
+        },
+        {
+          "id": "2026-08-30-12:30-35",
+          "day": "2026-08-30",
+          "startTime": "12:30",
           "endTime": "14:30",
           "title": "Lunch",
-          "description": "Enjoy a meal with fellow participants.",
-          "location": "Casa del Parco",
-          "color": "#458657"
+          "description": "",
+          "location": "",
+          "color": "#ff9900"
         },
         {
-          "id": "2026-08-28-14:30-21",
-          "day": "2026-08-28",
+          "id": "2026-08-30-14:30-36",
+          "day": "2026-08-30",
           "startTime": "14:30",
-          "endTime": "16:00",
-          "title": "Career Coffee",
-          "description": "Networking session with industry professionals.",
-          "location": "Town Hall",
+          "endTime": "00:00",
+          "title": "Goodbye!",
+          "description": "End of programme and departure",
+          "location": "",
           "color": "#fecc08"
         }
       ]
